74d8850e00cca2c90090b50fcba5cb48862eb6a6
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-01-22 19:52:57 +0100

    hoja 8 ej 3

--- unidad4/hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Pais.java
+++ unidad4/hoja8/ejercicio3/src/main/java/hoja8/ejercicio3/Pais.java
@@ -14,17 +14,17 @@ public class Pais {
 
     private String nombre;
     private static int ciudades;
-    private Ciudad[] cities;
+    private Ciudad[] c;
 
-    public Pais(String nombre) {
-        this.nombre = nombre;
-        this.cities = new Ciudad[25];
-        this.ciudades = 0;
+    public Pais(String _nombre) {
+        nombre = _nombre;
+        c = new Ciudad[25];
+        ciudades = 0;
     }
 
     public void insertar() {
         Scanner t;
-        if (cities.length <= ciudades) {
+        if (c.length <= ciudades) {
             System.out.printf("No podemos añadir mas ciudades a %s", this.nombre);
         } else {
             System.out.print("Introduce el nombre de la ciudad: ");
@@ -35,7 +35,7 @@ public class Pais {
             float lon = new Scanner(System.in).nextFloat();
             System.out.print("Introduce el numero de habitantes: ");
             int hab = new Scanner(System.in).nextInt();
-            cities[ciudades] = new Ciudad(nombre, lat, lon, hab);
+            c[ciudades] = new Ciudad(nombre, lat, lon, hab);
             ciudades++;
         }
     }
@@ -43,8 +43,8 @@ public class Pais {
     public Ciudad topHabitantes() {
         Ciudad top = new Ciudad("0", 0, 0, 0);
         for (int i = 0; i < ciudades; i++) {
-            if (cities[i].getHabitantes() > top.getHabitantes()) {
-                top = cities[i];
+            if (c[i].getHabitantes() > top.getHabitantes()) {
+                top = c[i];
             }
         }
         return top;
@@ -53,7 +53,7 @@ public class Pais {
     public boolean existe(String nombre) {
         boolean existe = false;
         for (int i = 0; i < ciudades; i++) {
-            if (cities[i].getNombre().equalsIgnoreCase(nombre)) {
+            if (c[i].getNombre().equalsIgnoreCase(nombre)) {
                 existe = true;
             }
         }
@@ -63,7 +63,7 @@ public class Pais {
     public int poblacionMedia() {
         int suma = 0;
         for (int i = 0; i < ciudades; i++) {
-            suma += cities[i].getHabitantes();
+            suma += c[i].getHabitantes();
         }
         return suma /= ciudades;
     }
@@ -71,7 +71,7 @@ public class Pais {
     public String toString() {
         String cadena = String.format("PAIS: %s\nNº de ciudades: %d\nCIUDADES: ", this.nombre, ciudades);
         for (int i = 0; i < ciudades; i++) {
-            cadena += cities[i].toString() + "\n\t";
+            cadena += c[i].toString() + "\n\t";
         }
         return cadena;
     }
