0e06332ec949b231a3bca21bdc2dc68ee55bbc31
ethanfr94 <izan.francoanduaga@iesmiguelherrero.com>
2024-01-30 15:16:46 +0100

    ud 5 h 1 ej 1

---
+++ ud5/ej1/src/main/java/h1/ej1/Ej1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package h1.ej1;
+
+/**
+ *
+ * @author DAM122
+ */
+public class Ej1 {
+
+    public static void main(String[] args) {
+        Pajaro p1 = new Pajaro();
+        Pajaro p2 = new Pajaro("azul", 3);
+        Pajaro.muestraPajaro();
+    }
+}
